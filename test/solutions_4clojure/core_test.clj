d85e36ba686641153f9d0721602ef7583d306d9a
Alexandre Karpov <akarpov@walmartlabs.com>
2014-05-23 15:08:17 -0700

    reverse and count tests

--- test/solutions_4clojure/core_test.clj
+++ test/solutions_4clojure/core_test.clj
@@ -1,6 +1,6 @@
 (ns solutions-4clojure.core-test
   (:require [clojure.test :refer :all]
-            [solutions-4clojure.core :refer :all]
+            [solutions-4clojure.easy :refer :all]
             [solutions-4clojure.elementary :refer :all]))
 
 (deftest test-134
@@ -11,4 +11,14 @@
   (testing "Creating a map with defaults"
     (is (= (sol-156 "foo" [:a :b :c])
            {:a "foo" :b "foo" :c "foo"}))))
-    
+
+(deftest test-my-count
+  (testing "Re-implementation of count"
+    (is (= (my-count [1 2 3 4]) 4))
+    (is (= (my-count []) 0))
+    (is (= (my-count {:a 1 :b 2} ) 2))))
+
+(deftest test-my-reverse
+  (testing "Reversing a seq"
+    (is (= (my-reverse [1 2 3]) [3 2 1]))
+    (is (= (= (my-reverse []) [])))))
